b0173016737597a1cfc239be1c81e495d926cb8e
KampfTracer <Franciscogabrielaguilera@gmail.com>
2023-07-28 23:16:57 -0400

    se incorporo  fetch ,Generación del DOM de forma dinámica, json, api y arreglo en css

---
+++ items.js
@@ -0,0 +1,18 @@
+[
+    {
+        "id":1,
+        "text":"ejemplo2",
+        
+    },
+    {
+        "id":2,
+        "text":"ejemplo2",
+        
+    },
+    {
+        "id":3,
+        "text":"ejemplo2",
+        
+    },
+   
+]   
--- main.js
+++ main.js
@@ -1,3 +1,47 @@
+// Elementos del DOM
+const monedaEl_one = document.getElementById('moneda-uno');
+const monedaEl_two = document.getElementById('moneda-dos');
+const cantidadEl_one = document.getElementById('cantidad-uno');
+const cantidadEl_two = document.getElementById('cantidad-dos');
+const cambioEl = document.getElementById('cambio');
+const tazaEl = document.getElementById('taza');
+
+// Función para calcular el cambio de moneda
+const calculate = async () => {
+  const moneda_one = monedaEl_one.value;
+  const moneda_two = monedaEl_two.value;
+
+  try {
+    const response = await fetch(`https://api.exchangerate-api.com/v4/latest/${moneda_one}`);
+    const data = await response.json();
+    const taza = data.rates[moneda_two];
+
+    cambioEl.innerText = `1 ${moneda_one} = ${taza} ${moneda_two}`;
+    cantidadEl_two.value = (cantidadEl_one.value * taza).toFixed(2);
+  } catch (error) {
+    console.log('Error al obtener el cambio de moneda:', error);
+    // Aquí podrías mostrar un mensaje de error al usuario si lo deseas.
+  }
+};
+
+// Event listeners para el cálculo del cambio de moneda
+monedaEl_one.addEventListener('change', calculate);
+cantidadEl_one.addEventListener('input', calculate);
+monedaEl_two.addEventListener('change', calculate);
+cantidadEl_two.addEventListener('input', calculate);
+
+tazaEl.addEventListener('click', () => {
+  const temp = monedaEl_one.value;
+  monedaEl_one.value = monedaEl_two.value;
+  monedaEl_two.value = temp;
+  calculate();
+});
+
+// Función para mostrar mensajes en el DOM
+function mostrarMensaje(mensaje) {
+  document.getElementById("mensaje").textContent = mensaje;
+}
+
 // Crear la clase Usuario
 class Usuario {
   constructor(nombre, apellido, email, edad, montoPrestamo, plazo, tasaInteres, cuotaMensual, dni) {
